3d0a3bdcb48bea5a3b7c3dbe859d3a16cd8e1b45
0razzu <0razzu@protonmail.com>
2022-02-17 00:45:02 +0600

    jar can be created now

--- lab2-1/src/main/java/checkers/CheckersServer.java
+++ lab2-1/src/main/java/checkers/CheckersServer.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheckersServer {
     public static void main(String[] args) {
-        SpringApplication.run(CheckersServer.class);
+        SpringApplication.run(CheckersServer.class, args);
     }
 }
